01f8ebad903a6ef199a9f74626e9076e648d64dc
Tiffany Poss <tiffanyposs@gmail.com>
2020-04-08 22:19:31 -0400

    setup mongo atlas

--- server/server.js
+++ server/server.js
@@ -5,19 +5,30 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
 
+require('dotenv').config();
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-b4ijs.mongodb.net/test?retryWrites=true&w=majority`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once('open', () => console.log('Connected to MongoDB instance.'))
+  .on('error', error => console.log('Error connecting to MongoDB:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
